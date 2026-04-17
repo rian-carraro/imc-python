1dbd2dd312aa1872dfc6e35a1acc120731338560
Rian Carraro <riancarraro@gmail.com>
2026-04-16 22:10:44 -0300

    criando banco de dados

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from db import execute_query
 
 app = Flask(__name__)
 app.secret_key = 'imc_secret_key_2026'
@@ -6,6 +7,10 @@ app.secret_key = 'imc_secret_key_2026'
 
 @app.route('/')
 def index():
+    sql = "SHOW TABLES"
+    resultado = execute_query(sql, fetch=True)
+    print(resultado)
+
     return render_template('index.html')
 
 
@@ -17,6 +22,32 @@ def resultados():
 
 @app.route('/calcular', methods=['GET', 'POST'])
 def calcular():
+
+    if request.method == 'POST':
+        nome = request.form.get('nome', 'Não foi enviado nome!')
+        peso = request.form.get('peso', 'Não foi enviado peso!')
+        altura = request.form.get('altura', 'Não foi enviado altura!')
+
+        peso = float(peso)
+        altura = float(altura)
+
+        imc = round(peso / (altura ** 2), 2) 
+
+        if imc < 18.5:
+            classificacao = 'Abaixo do peso'
+        elif imc < 25:
+            classificacao = 'Peso normal'
+        elif imc < 30:
+            classificacao = 'Sobrepeso'
+
+        ##demais classificações
+        else:
+            classificacao = 'Erro na classificação'
+
+        flash(f'Olá {nome}, seu IMC é {imc} e sua classificação é: {classificacao}', 'success')      
+
+        return redirect(url_for('resultados'))
+
     return render_template('formulario.html')
 
 
---
+++ db.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from mysql.connector import Error, pooling
+
+DB_PARAMS = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '',
+    'database': 'imc_python',
+    'charset': 'utf8mb4',
+    'time_zone': '-03:00',
+    'use_pure': True,
+    'connect_timeout': 10
+}
+
+_pool = pooling.MySQLConnectionPool(
+    pool_name='imc_pool',
+    pool_size=5, 
+    **DB_PARAMS
+    )
+
+def get_connection():
+    try:
+        return _pool.get_connection()
+
+    except Error as e:
+        raise Exception(f"Erro ao obter conexão do pool: {e}")
+    
+def execute_query(sql, params=None, fetch=False):
+    conn = get_connection()
+    try:
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute(sql, params or ())
+
+        if fetch:
+            return cursor.fetchall()
+        else:
+            conn.commit()
+            return cursor.rowcount
+        
+    except Error as e:
+        conn.rollback()
+        raise Exception(f"Erro ao executar query: {e}")
+    finally:
+        cursor.close()
+        conn.close()
